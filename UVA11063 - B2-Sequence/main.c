2e13783fae640343169852ad4980e4debc288150
Andy Yang <23339548+a962702@users.noreply.github.com>
2021-10-20 23:29:52 +0800

    Fix UVA11063 - C version

--- UVA11063 - B2-Sequence/main.c
+++ UVA11063 - B2-Sequence/main.c
@@ -5,14 +5,16 @@ int main(){
 	int caseid = 1, N;
 	while(scanf("%d", &N)!=EOF){	
 		int b[100];
-		bool set[20001] = {true}, ans = true;
-		for(int i = 0; i < N; i++){
+		bool set[20001] = {false}, ans = true;
+		for(int i = 0; i < N; i++)
 			scanf("%d", &b[i]);
-			if(i > 0)
-				if(set[b[i] - b[i - 1]] == true)
+		for(int i = 0; i < N && ans; i++){
+			for(int j = i; j < N && ans; j++){
+				if(b[i] > b[j] || b[i] < 1 || b[j] < 1 || set[b[i] + b[j]])
 					ans = false;
 				else
-					set[b[i] - b[i - 1]] = true;
+					set[b[i] + b[j]] = true;
+			}
 		}
 		if(ans)
 			printf("Case #%d: It is a B2-Sequence.\n\n", caseid++);
