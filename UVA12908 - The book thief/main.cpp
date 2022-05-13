3013be4f68ec4597cfb39858907f40c7135b78ac
Andy Yang <23339548+a962702@users.noreply.github.com>
2022-05-13 21:39:33 +0800

    Fix UVA12908 - CPP version

--- UVA12908 - The book thief/main.cpp
+++ UVA12908 - The book thief/main.cpp
@@ -1,15 +1,16 @@
-// 2022-05-13 07:24:59 - Time limit exceeded
+// Reference: https://cpe.cse.nsysu.edu.tw/cpe/file/attendance/problemPdf/12908.php
 #include <iostream>
+#include <vector>
+#include <algorithm>
 using namespace std;
 
 int main(){
-	int i, in, table[15000] = {0};
-	for(i = 1; i < 15000; i++)
-		table[i] = table[i - 1] + i;
+	int i, in;
+	vector<int> v;
+	for(i = 0; i < 15000; i++)
+		v.push_back(i * (i + 1) / 2);
 	while(cin >> in && in){
-		i = 2;
-		while(table[i] <= in)
-			i++;
-		cout << table[i] - in << " " << i << endl;
+		vector<int>::iterator it = upper_bound(v.begin(), v.end(), in);
+		cout << *it - in << " " << it - v.begin() << endl;
 	}
 }
